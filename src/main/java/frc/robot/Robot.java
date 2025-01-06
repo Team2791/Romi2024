f6f852509ea5681bf4d63366626fa7219fecc79f
rikhilkatpally1 <rikhilkatpally1@gmail.com>
2025-01-06 18:14:13 -0500

    bumneededhelp

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -31,7 +32,12 @@ public class Robot extends TimedRobot {
      * that you want ran during disabled, autonomous, teleoperated and test.
      */
     @Override
-    public void robotPeriodic() {}
+    public void robotPeriodic() {
+
+
+        CommandScheduler.getInstance().run();
+
+    }
 
     /**
      * This function is called once when autonomous is enabled.
